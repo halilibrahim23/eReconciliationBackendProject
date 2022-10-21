d2c78370343584cee4f2b5100eb8c343e41a4d69
halilibrahim23 <haliliibrahim17@gmail.com>
2022-10-22 01:58:28 +0300

    GenericEkledni

---
+++ DataAccess/Concrete/EntityFramework/Context/ContextDb.cs
@@ -0,0 +1,38 @@
+﻿// optionsBuilder.UseSqlServer(@"Server=DESKTOP-8020TLA;Database=eReconciliationDb;Integrated Security=true");
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-8020TLA;Database=eReconciliationDb;Integrated Security=true");
+        }
+
+        public DbSet<AccountReconciliationDetail> AccountReconciliationDetails { get; set; }
+        public DbSet<AccountReconciliation> AccountReconciliations { get; set; }
+        public DbSet<BaBsReconciliation> BaBsReconciliations { get; set; }
+        public DbSet<BaBsReconciliationDetail> BaBsReconciliationDetails { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        public DbSet<MailParameter> MailParameters { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+       // public DbSet<MailTemplate> MailTemplates { get; set; }
+       // public DbSet<TermsandCondition> TermsandConditions { get; set; }
+       //public DbSet<ForgotPassword> ForgotPasswords { get; set; }
+        //public DbSet<UserRelationShip> UserReletionships { get; set; }
+       // public DbSet<UserThemeOption> UserThemeOptions { get; set; }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfAccountReconciliationDal.cs
@@ -0,0 +1,159 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfAccountReconciliationDal : EfEntityRepositoryBase<AccountReconciliation, ContextDb>, IAccountReconciliationDal
+    {
+        /* public List<AccountReconciliationDto> GetAllDto(int companyId)
+         {
+             using (var context = new ContextDb())
+             {
+                 var result = from recoinciliation in context.AccountReconciliations.Where(p => p.CompanyId == companyId)
+                              join company in context.Companies on recoinciliation.CompanyId equals company.Id
+                              join account in context.CurrencyAccounts on recoinciliation.CurrencyAccountId equals account.Id
+                              join currency in context.Currencies on recoinciliation.CurrencyId equals currency.Id
+                              select new AccountReconciliationDto
+                              {
+                                  CompanyId = companyId,
+                                  CurrencyAccountId = account.Id,
+                                  AccountIdentityNumber = account.IdentityNumber,
+                                  AccountName = account.Name,
+                                  AccountTaxDepartment = account.TaxDepartment,
+                                  AccountTaxIdNumber = account.TaxIdNumber,
+                                  CompanyIdentityNumber = company.IdentityNumber,
+                                  CompanyName = company.Name,
+                                  CompanyTaxDepartment = company.TaxDepartment,
+                                  CompanyTaxIdNumber = company.TaxIdNumber,
+                                  CurrencyCredit = recoinciliation.CurrencyCredit,
+                                  CurrencyDebit = recoinciliation.CurrencyDebit,
+                                  CurrencyId = recoinciliation.CurrencyId,
+                                  EmailReadDate = recoinciliation.EmailReadDate,
+                                  EndingDate = recoinciliation.EndingDate,
+                                  Guid = recoinciliation.Guid,
+                                  Id = recoinciliation.Id,
+                                  IsEmailRead = recoinciliation.IsEmailRead,
+                                  IsResultSucceed = recoinciliation.IsResultSucceed,
+                                  IsSendEmail = recoinciliation.IsSendEmail,
+                                  ResultDate = recoinciliation.ResultDate,
+                                  ResultNote = recoinciliation.ResultNote,
+                                  SendEmailDate = recoinciliation.SendEmailDate,
+                                  StartingDate = recoinciliation.StartingDate,
+                                  CurrencyCode = currency.Code,
+                                  AccountEmail = account.Email,
+                                  AccountCode = account.Code
+                              };
+                 return result.ToList();
+             }
+         }
+
+         public AccountReconciliationDto GetByCodeDto(string code)
+         {
+             using (var context = new ContextDb())
+             {
+                 var result = from recoinciliation in context.AccountReconciliations.Where(p => p.Guid == code)
+                              join company in context.Companies on recoinciliation.CompanyId equals company.Id
+                              join account in context.CurrencyAccounts on recoinciliation.CurrencyAccountId equals account.Id
+                              join currency in context.Currencies on recoinciliation.CurrencyId equals currency.Id
+                              select new AccountReconciliationDto
+                              {
+                                  CompanyId = recoinciliation.CompanyId,
+                                  CurrencyAccountId = account.Id,
+                                  AccountIdentityNumber = account.IdentityNumber,
+                                  AccountName = account.Name,
+                                  AccountTaxDepartment = account.TaxDepartment,
+                                  AccountTaxIdNumber = account.TaxIdNumber,
+                                  CompanyIdentityNumber = company.IdentityNumber,
+                                  CompanyName = company.Name,
+                                  CompanyTaxDepartment = company.TaxDepartment,
+                                  CompanyTaxIdNumber = company.TaxIdNumber,
+                                  CurrencyCredit = recoinciliation.CurrencyCredit,
+                                  CurrencyDebit = recoinciliation.CurrencyDebit,
+                                  CurrencyId = recoinciliation.CurrencyId,
+                                  EmailReadDate = recoinciliation.EmailReadDate,
+                                  EndingDate = recoinciliation.EndingDate,
+                                  Guid = recoinciliation.Guid,
+                                  Id = recoinciliation.Id,
+                                  IsEmailRead = recoinciliation.IsEmailRead,
+                                  IsResultSucceed = recoinciliation.IsResultSucceed,
+                                  IsSendEmail = recoinciliation.IsSendEmail,
+                                  ResultDate = recoinciliation.ResultDate,
+                                  ResultNote = recoinciliation.ResultNote,
+                                  SendEmailDate = recoinciliation.SendEmailDate,
+                                  StartingDate = recoinciliation.StartingDate,
+                                  CurrencyCode = currency.Code,
+                                  AccountEmail = account.Email,
+                                  AccountCode = account.Code
+                              };
+                 return result.FirstOrDefault();
+             }
+         }
+
+         public AccountReconciliationDto GetByIdDto(int id)
+         {
+             using (var context = new ContextDb())
+             {
+                 var result = from recoinciliation in context.AccountReconciliations.Where(p => p.Id == id)
+                              join company in context.Companies on recoinciliation.CompanyId equals company.Id
+                              join account in context.CurrencyAccounts on recoinciliation.CurrencyAccountId equals account.Id
+                              join currency in context.Currencies on recoinciliation.CurrencyId equals currency.Id
+                              select new AccountReconciliationDto
+                              {
+                                  CompanyId = recoinciliation.CompanyId,
+                                  CurrencyAccountId = account.Id,
+                                  AccountIdentityNumber = account.IdentityNumber,
+                                  AccountName = account.Name,
+                                  AccountTaxDepartment = account.TaxDepartment,
+                                  AccountTaxIdNumber = account.TaxIdNumber,
+                                  CompanyIdentityNumber = company.IdentityNumber,
+                                  CompanyName = company.Name,
+                                  CompanyTaxDepartment = company.TaxDepartment,
+                                  CompanyTaxIdNumber = company.TaxIdNumber,
+                                  CurrencyCredit = recoinciliation.CurrencyCredit,
+                                  CurrencyDebit = recoinciliation.CurrencyDebit,
+                                  CurrencyId = recoinciliation.CurrencyId,
+                                  EmailReadDate = recoinciliation.EmailReadDate,
+                                  EndingDate = recoinciliation.EndingDate,
+                                  Guid = recoinciliation.Guid,
+                                  Id = recoinciliation.Id,
+                                  IsEmailRead = recoinciliation.IsEmailRead,
+                                  IsResultSucceed = recoinciliation.IsResultSucceed,
+                                  IsSendEmail = recoinciliation.IsSendEmail,
+                                  ResultDate = recoinciliation.ResultDate,
+                                  ResultNote = recoinciliation.ResultNote,
+                                  SendEmailDate = recoinciliation.SendEmailDate,
+                                  StartingDate = recoinciliation.StartingDate,
+                                  CurrencyCode = currency.Code,
+                                  AccountEmail = account.Email,
+                                  AccountCode = account.Code
+                              };
+                 return result.FirstOrDefault();
+             }
+         }
+
+         public AccountReconciliationsCountDto GetCountDto(int companyId)
+         {
+             using (var context = new ContextDb())
+             {
+                 var result = context.AccountReconciliations.Where(p => p.CompanyId == companyId).ToList();
+                 AccountReconciliationsCountDto accountReconciliationsCountDto = new AccountReconciliationsCountDto
+                 {
+                     AllReconciliation = result.Count(),
+                     SucceedReconciliation = result.Where(p => p.IsResultSucceed == true).Count(),
+                     NotResponseReconciliation = result.Where(p => p.IsResultSucceed == null).Count(),
+                     FailReconciliation = result.Where(p => p.IsResultSucceed == false).Count()
+                 };
+                 return accountReconciliationsCountDto;
+             }
+         }
+     }
+ }*/
+    }
+}
